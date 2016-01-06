d83b4f5442125d4b7845ab41484fbdfef22b8396
Jakob Misbach <squirlemaster@gmail.com>
2016-01-06 08:46:57 -0500

    Update 6_29.py

--- 6_29.py
+++ 6_29.py
@@ -1,17 +1,11 @@
 def isValid(number):
-    print("isValid")
+    if getSize(number) >= 13 and getSize(number) <= 16 and (prefixMatched(number, 4) or prefixMatched(number, 5) or prefixMatched(number, 6) or prefixMatched(number, 37)) and (sumOfDoubleEvenPlace(number) + sumOfOddPlace(number)) % 10 == 0:
+        return True
+    else:
+        return False
 
-def sumOfDoubleEvenPlace(number):
-    total = 0
-    numString = str(number)
-    size = getSize(number)
-
-    for i in range(0, size, 2):
-        num = int(numString[i])
-        editNum = getDigit(num * 2)
-        total += editNum
-
-    return total    
+def getSize(d):
+    return len(str(d))
 
 def getDigit(number):
     isSingle = 0
@@ -21,26 +15,6 @@ def getDigit(number):
         else:
             number = number // 10 + number % 10
     return number
-
-def sumOfOddPlace(number):
-    total = 0
-    numString = str(number)
-    size = getSize(number)
-
-    for i in range(1, size, 2):
-        num = int(numString[i])
-        total += num
-
-    return total 
-
-def prefixMatched(number, d):
-    if getPrefix(number, getSize(d)) == d:
-        True
-    else:
-        False
-
-def getSize(d):
-    return len(str(d))
 
 def getPrefix(number, k):
     if getSize(number) <= k:
@@ -57,7 +31,36 @@ def getPrefix(number, k):
             i -= 1
         return prefix
 
+def sumOfOddPlace(number):
+    total = 0
+    numString = str(number)
+    size = getSize(number)
+    for i in range(1, size, 2):
+        num = int(numString[i])
+        total += num
+    return total
+
+def sumOfDoubleEvenPlace(number):
+    total = 0
+    numString = str(number)
+    size = getSize(number)
+    for i in range(0, size, 2):
+        num = int(numString[i])
+        editNum = getDigit(num * 2)
+        total += editNum
+    return total
+
+def prefixMatched(number, d):
+    if getPrefix(number, getSize(d)) == d:
+        return True
+    else:
+        return False
+
 def main():
-    print("main")
+    creditCard = eval(input("Enter a credit card number to be tested here: "))
+    if isValid(creditCard):
+        print("That credit card is valid.")
+    else:
+        print("That credit card is invalid.")
 
 main()
